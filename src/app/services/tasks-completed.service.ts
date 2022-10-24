c5afac20fc164bf4e73ac657d19226d83b30eacf
Jazmin Ochoa Benitez <jazmin_2303@hotmail.com>
2022-10-23 23:04:35 -0600

    2do Tab

---
+++ src/app/services/tasks-completed.service.ts
@@ -0,0 +1,26 @@
+import { TasksService } from './tasks.service';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TasksCompletedService {
+   public tasksCompleted: string[] = [];
+
+   constructor(private TasksService:TasksService) {
+    this.tasksCompleted=TasksService.returnTaskC();
+   }
+
+
+  public getTasks():string[] {
+    return this.tasksCompleted;
+ }
+
+ public addTasks(task:string){
+  this.tasksCompleted.push(task);
+}
+
+public removeTask(pos:number){
+  this.tasksCompleted.splice(pos, 1);
+}
+}
